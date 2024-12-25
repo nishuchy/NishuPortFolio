5a22cc400bc7470678ed8e2d1dde507d2fe6613f
Nishu <nishuchy11@gmail.com>
2024-12-25 18:26:42 +0600

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using NishuPortFolio.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NishuPortFolio.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            ViewData["ActiveMenu"] = "Home";
+           
+            return View();
+        }
+        public IActionResult ProjectDetails()
+        {
+            ViewData["ActiveMenu"] = "Projects";
+
+            return View();
+
+        }
+        public IActionResult Contact()
+        {
+            ViewData["ActiveMenu"] = "Contact";
+            return View();
+
+        }
+        
+        public IActionResult Research()
+        {
+            ViewData["ActiveMenu"] = "Research";
+            return View();
+        }
+        public IActionResult Projects()
+        {
+            ViewData["ActiveMenu"] = "Projects";
+            return View();
+        }
+        
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
